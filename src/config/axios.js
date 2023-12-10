7ce51cc4f61bc2488a925a4447871c12b1e473e2
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-12-10 13:00:03 +0100

    Cambiando logica para obetener el token

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,15 +1,16 @@
 import axios from "axios";
-// 
+//
 
-const apiBackend = import.meta.env.VITE_BASE_URL
+const apiBackend = import.meta.env.VITE_BASE_URL;
 const clienteAxios = axios.create({
     baseURL: apiBackend
 });
 clienteAxios.interceptors.request.use(config => {
+    config.headers = {
+        ...config.headers,
+        Authorization: `Bearer ${localStorage.getItem("token")}`
+    };
 
-    if (window.localStorage.token) {
-        config.headers.Authorization = `Bearer ${window.localStorage.token}`; // eslint-disable-line no-param-reassign
-    }
     return config;
 });
 // }, err => Promise.reject(err)); // Do something with request error
