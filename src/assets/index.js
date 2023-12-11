5c6b9a00934b8ac3d3d64ccd5ded79843a16e07d
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2023-12-11 22:08:46 +0100

    Añadiendo imagenes y svg a carpeta assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,6 +5,11 @@ import TableList from "./svg/table-list.svg";
 import Delete from "./svg/delete.svg";
 import Edit from "./svg/edit.svg";
 import Pdf from "./svg/pdf.svg";
+import LogoutBtn from "./svg/logoutBtn.svg";
+import CartNav from "./svg/cartNav.svg";
+import UsersNav from "./svg/usersNav.svg";
+import TableListNav from "./svg/table-listNav.svg";
+import CreateOrder from "./svg/create.svg";
 
 export {
     Home,
@@ -13,5 +18,10 @@ export {
     TableList,
     Delete,
     Edit,
-    Pdf
+    Pdf,
+    LogoutBtn,
+    CartNav,
+    UsersNav,
+    TableListNav,
+    CreateOrder
 };
