19021276f07283dc42e0df12644d3cbfc72f40b9
Lizandro Narváez <lizandrojesus13@hotmail.com>
2024-04-27 23:31:55 +0200

    RUTAS PRIVADAS Y PUBLICAS PARA PERMITIR O DENEGAR EL ACCESO A TRAVES DEL TOKEN

--- src/components/routes/CrmRoutePrivate.jsx
+++ src/components/routes/CrmRoutePrivate.jsx
@@ -14,15 +14,15 @@ export const CrmRoutePrivate = () => {
                     <Route exact path="/dashboard" element={<Dashboard />} />
                     <Route exact path="/clientes" element={<ClientsAll />} />
                     <Route exact path='/clientes/nuevo-cliente' element={<ClientCreate />} />
-                    <Route exact path='/clientes/editar/:_id' element={<ClientUpdate />} />
+                    <Route exact path='/clientes/editar/:id' element={<ClientUpdate />} />
 
                     <Route exact path="/productos" element={<ProductsAll />} />
                     <Route exact path="/productos/nuevo-producto" element={<ProductCreate />} />
-                    <Route exact path="/productos/editar-producto/:_id" element={<ProductsUpdate />} />
+                    <Route exact path="/productos/editar-producto/:id" element={<ProductsUpdate />} />
 
                     <Route exact path="/pedidos/clientes" element={<OrderAll />} />
-                    <Route exact path="/pedidos/nuevo/:_id" element={<OrderCreate />} />
-                    <Route exact path="/pedidos/pdf/:_idPedido" element={<OrderPDF />} />
+                    <Route exact path="/pedidos/nuevo/:id" element={<OrderCreate />} />
+                    <Route exact path="/pedidos/pdf/:idPedido" element={<OrderPDF />} />
 
                     <Route path="/*" element={<Navigate to={"/dashboard"} />} />
                 </Routes>
--- src/components/routes/CrmRoutePublic.jsx
+++ src/components/routes/CrmRoutePublic.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Home } from "../Pages";
 import { Login, Register } from "../../auth";
-import { ResetPassword } from "../../auth/ResetPassword/ResetPassword";
 import { Route, Routes } from "react-router-dom";
 import { NotFound } from "../notFound/NotFound";
 export const CrmRoutePublic = () => {
@@ -12,8 +11,7 @@ export const CrmRoutePublic = () => {
                     <Route path='/' element={<Home />} />
                     <Route path='/login' element={<Login />} />
                     <Route path='/register' element={<Register />} />
-                    <Route path='/password-reset' element={<ResetPassword />} />
-                    <Route path="*" element={<NotFound />} />
+                    <Route path="/*" element={<NotFound />} />
                 </Routes>
             </div>
         </>
