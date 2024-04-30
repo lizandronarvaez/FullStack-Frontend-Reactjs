4e59840f8adad1339b90e8753d1364e381989af6
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2024-04-30 19:09:07 +0200

    Fix: Cambio de nombre variable conexion de api backend ( Axios)

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,12 +2,11 @@ import axios from "axios";
 import { getEnv } from "../helpers/getEnv";
 const { VITE_BASE_URL } = getEnv();
 
-export const clienteAxios = axios.create({ baseURL: VITE_BASE_URL });
-clienteAxios.interceptors.request.use(config => {
+export const springBootAxios = axios.create({ baseURL: VITE_BASE_URL });
+springBootAxios.interceptors.request.use(config => {
     config.headers = {
         ...config.headers,
         Authorization: `Bearer ${localStorage.getItem("token")}`
     };
-
     return config;
 });
