5654c7db2825edf26bfebf043954d7508aed2938
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2024-04-30 19:13:51 +0200

    Feat: Actualizando componentes

--- src/components/orders/SearchProducts/SearchProducts.jsx
+++ src/components/orders/SearchProducts/SearchProducts.jsx
@@ -1,34 +1,25 @@
 import React, { useEffect, useState } from "react";
 import "./SearchProducts.css";
-import { clienteAxios } from "../../../api/axios";
+import { springBootAxios } from "../../../api/axios";
 import { ListTableSearch } from "./ListTableSearch/ListTableSearch";
-export const SearchProducts = ({ buscarProducto, onAddProduct }) => {
+export const SearchProducts = ({ onAddProduct }) => {
     const [searchProduct, setSearchProduct] = useState("");
     const [dbProducts, setdbProducts] = useState([]);
-    const [dbProductFiltered, setdbProductsFiltered] = useState([]);
-
     const valorInput = ({ target: { value } }) => setSearchProduct(value);
-    const SearchValueClean = searchProduct.trim().toLowerCase();
+    const searchValueClean = searchProduct.trim().toLowerCase();
 
     const onSearchProductDB = async () => {
-        if (SearchValueClean < 1) return;
-        const { data } = await clienteAxios.get(`/products/clientes/${SearchValueClean}`);
+        if (searchValueClean < 1) return;
+        const { data } = await springBootAxios.get("/products");
         setdbProducts(data);
     };
 
-    const filteredProducts = (e) => {
-        const productsFilter = dbProducts.filter((product) => {
-            const productName = product.fullname.toLowerCase();
-            const brandName = product.fullname.toLowerCase();
-            return productName.includes(SearchValueClean) || brandName.includes(SearchValueClean) || null;
-        }
-        );
-        setdbProductsFiltered(productsFilter);
+    const closeDivSearch = () => {
+        setSearchProduct("");
+        setdbProducts([]);
     };
-    useEffect(() => {
-        onSearchProductDB();
-        filteredProducts();
-    }, [searchProduct]);
+
+    useEffect(() => { onSearchProductDB(); }, [searchProduct]);
     return (
         <>
             <form className="search-products">
@@ -36,15 +27,17 @@ export const SearchProducts = ({ buscarProducto, onAddProduct }) => {
                 <div >
                     <div className="campo">
                         <label>Productos:</label>
-                        <input type="text"
+                        <input
+                            type="text"
                             placeholder="Busca el producto"
                             name="productos"
                             onChange={valorInput}
+                            value={searchProduct}
                         />
                     </div>
                     {
                         searchProduct.length > 1
-                            ? < ListTableSearch dbProductFiltered={dbProductFiltered} onAddProduct={onAddProduct} />
+                            ? < ListTableSearch dbProducts={dbProducts} onAddProduct={onAddProduct} closeDivSearch={closeDivSearch} />
                             : null
                     }
                 </div>
