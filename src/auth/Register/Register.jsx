b3aa81b10e35a729033356499370bfb8093f223d
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2024-04-30 19:10:09 +0200

    Fix: Refactorizando estructura jsx

--- src/auth/Register/Register.jsx
+++ src/auth/Register/Register.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from "react-router-dom";
 import "./Register.css";
 import { useState } from "react";
 import Swal from "sweetalert2/dist/sweetalert2.all";
-import { clienteAxios } from "../../api/axios";
+import { springBootAxios } from "../../api/axios";
 
 const formValues = {
     fullname: "",
@@ -17,57 +17,34 @@ const Register = () => {
     const { fullname, email, password, passwordRepeat } = dataForm;
     const registerData = ({ target }) => { setDataForm({ ...dataForm, [target.name]: target.value }); };
     const onSubmitRegister = async (e) => {
+        // Limpieza de datos
+        dataForm.fullname = dataForm.fullname.trim().toLowerCase();
+        dataForm.email = dataForm.email.trim().toLowerCase();
+
         e.preventDefault();
         const inputsValues = Object.values(dataForm);
         for (const input of inputsValues) {
             if (input.length < 1) {
-                Swal.fire({
-                    position: "center",
-                    icon: "error",
-                    title: "Completa los campos",
-                    showConfirmButton: false,
-                    timer: 1500
-                });
+                Swal.fire({ position: "center", icon: "error", title: "Completa los campos", showConfirmButton: false, timer: 1500 });
                 return;
             }
         }
         if (password !== passwordRepeat) {
-            Swal.fire({
-                position: "center",
-                icon: "error",
-                title: "Las contraseñas no son iguales",
-                showConfirmButton: false,
-                timer: 1500
-            });
+            Swal.fire({ position: "center", icon: "error", title: "Las contraseñas no son iguales", showConfirmButton: false, timer: 1500 });
             return;
         }
-        const dataCleanForm = {
-            fullname: fullname.trim().toLowerCase(),
-            email: email.trim().toLowerCase(),
-            password
-        };
 
         try {
-            const { data } = await clienteAxios.post("/auth/register", dataCleanForm);
-            Swal.fire({
-                position: "center",
-                icon: "success",
-                title: data?.message,
-                showConfirmButton: false,
-                timer: 1500
-            });
-            setDataForm({
-                fullname: "",
-                email: "",
-                password: "",
-                passwordRepeat: ""
-            });
+            const { data } = await springBootAxios.post("/auth/register", dataForm);
+            Swal.fire({ position: "center", icon: "success", title: data?.message, showConfirmButton: false, timer: 1500 });
+            // setDataForm({ fullname: "", email: "", password: "", passwordRepeat: "" });
             navigate("/login");
         } catch (error) {
             Swal.fire({
                 position: "center",
                 icon: "error",
-                title: error.response.data.error || "Hubo un error en el registro",
+                title: "Hubo un error en el registro",
+                text: error?.response?.data,
                 showConfirmButton: false,
                 timer: 1500
             });
@@ -82,28 +59,43 @@ const Register = () => {
                     <h2>Registrar cuenta</h2>
                     <div className='campo'>
                         <label htmlFor="fullname">Nombre</label>
-                        <input type="text" name="fullname" autoComplete='on' value={fullname}
+                        <input
+                            type="text"
+                            name="fullname"
+                            autoComplete='on'
+                            value={fullname}
                             onChange={registerData}
                             placeholder="Introduce tu nombre completo"
                         />
                     </div>
                     <div className='campo'>
                         <label htmlFor="email">Email</label>
-                        <input type="email" name="email" autoComplete='on' value={email}
+                        <input
+                            type="email"
+                            name="email"
+                            autoComplete='on'
+                            value={email}
                             onChange={registerData}
                             placeholder="Introduce tu email"
                         />
                     </div>
                     <div className='campo'>
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" autoComplete="current-password"
+                        <input
+                            type="password"
+                            name="password"
+                            autoComplete="current-password"
                             value={password} onChange={registerData}
                             placeholder="Introduce una contraseña segura"
                         />
                     </div>
                     <div className='campo'>
                         <label htmlFor="passwordRepeat">Repetir Password</label>
-                        <input type="password" name="passwordRepeat" autoComplete="current-password" value={passwordRepeat}
+                        <input
+                            type="password"
+                            name="passwordRepeat"
+                            autoComplete="current-password"
+                            value={passwordRepeat}
                             onChange={registerData}
                             placeholder="Vuelve a introducir tu contraseña por seguridad"
                         />
