b794793a5103ec986d4717fb215c987215ed098c
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2024-04-30 19:11:12 +0200

    Fix: Actalizando archivos css y jsx.

--- src/components/clients/ClientCreate/ClientCreate.jsx
+++ src/components/clients/ClientCreate/ClientCreate.jsx
@@ -1,44 +1,40 @@
 import React, { useEffect, useState } from "react";
-import { clienteAxios } from "../../../api/axios";
-import Swal from "sweetalert2/dist/sweetalert2.all";
-import { useNavigate } from "react-router-dom";
+import { springBootAxios } from "../../../api/axios";
 import "./ClientCreate.css";
-const dataFormClient = {
+import { handleErrorResponse, handleSuccessResponse } from "../utils/handleResponse";
+import { useNavigate } from "react-router-dom";
+const formData = {
     fullname: "",
-    company: "",
     email: "",
-    phone: ""
+    phone: "",
+    address: "",
+    city: "",
+    country: "",
+    postalCode: ""
 };
 const NuevoCLiente = () => {
     const navigate = useNavigate();
-    const [cliente, setClientes] = useState(dataFormClient);
-    const datosFormulario = ({ target: { name, value } }) => { setClientes({ ...cliente, [name]: value }); };
+    const [cliente, setClientes] = useState(formData);
+    const datosFormulario = ({ target: { name, value } }) => {
+        setClientes({ ...cliente, [name]: value });
+    };
     const enviarFormulario = async (e) => {
         e.preventDefault();
-        const { data, status } = await clienteAxios.post("/clients", cliente);
-        if (status !== 201) {
-            Swal.fire({
-                title: "El cliente ya esta esta registrado o existe",
-                text: "Hubo un conflicto de datos",
-                icon: "error"
-            });
-            return;
+        try {
+            const { data, status } = await springBootAxios.post("/clients", cliente);
+            handleSuccessResponse(data, status);
+            navigate("/clientes");
+        } catch (err) {
+            handleErrorResponse(err);
         }
-        Swal.fire(data.mensaje, "", "success");
-        navigate("/clientes");
     };
 
-    // funcion para validar el formulario
-    const validarFormulario = () => {
-        const { fullname, company, email, phone } = cliente;
-        const validar = !fullname.length || !company.length || !email.length || !phone.length;
-        return validar;
-    };
     useEffect(() => { }, [cliente]);
     return (
         <>
             <div className="create-client">
                 <h2>Crear Nuevo Cliente</h2>
+
                 <form className="form_create_client" onSubmit={enviarFormulario}>
                     <legend>Campos obligatorios*</legend>
                     <div className="campo">
@@ -46,19 +42,32 @@ const NuevoCLiente = () => {
                         <input type="text" placeholder="Nombre" name="fullname" onChange={datosFormulario} />
                     </div>
                     <div className="campo">
-                        <label htmlFor="company">Empresa*</label>
-                        <input type="text" placeholder="Empresa" name="company" onChange={datosFormulario} />
-                    </div>
-                    <div className="campo">
-                        <label htmlFor="email">Email*</label>
-                        <input type="email" placeholder="Email" name="email" onChange={datosFormulario} />
+                        <label htmlFor="company">Email*</label>
+                        <input type="text" placeholder="Email" name="email" onChange={datosFormulario} />
                     </div>
                     <div className="campo">
                         <label htmlFor="phone">Teléfono*</label>
                         <input type="text" placeholder="Teléfono" name="phone" onChange={datosFormulario} />
                     </div>
                     <div className="campo">
-                        <input type="submit" className="buttonSubmit" value="Crear cliente" disabled={validarFormulario()} />
+                        <label htmlFor="phone">Dirección*</label>
+                        <input type="text" placeholder="Dirección" name="address" onChange={datosFormulario} />
+                    </div>
+                    <div className="campo">
+                        <label htmlFor="phone">Ciudad*</label>
+                        <input type="text" placeholder="Ciudad" name="city" onChange={datosFormulario} />
+                    </div>
+                    <div className="campo">
+                        <label htmlFor="phone">País*</label>
+                        <input type="text" placeholder="Teléfono" name="country" onChange={datosFormulario} />
+                    </div>
+                    <div className="campo">
+                        <label htmlFor="phone">Codigo Postal*</label>
+                        <input type="text" placeholder="Código Postal" name="postalCode" onChange={datosFormulario} />
+                    </div>
+                    <div className="campo campos-submit">
+                        <input type="submit" className="buttonSubmit" value="Crear cliente" />
+                        <input type="submit" className="cancelSubmit" value="Volver" onClick={() => navigate("/clientes")} />
                     </div>
                 </form>
             </div>
