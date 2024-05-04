08cb4ef23c7315301f9f423ceda46b6a38b5d37d
Lizandro <lizandrojesus13@hotmail.com>
2024-05-04 21:59:48 +0200

    Fix: Solucionando envio de token

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,10 +3,11 @@ import { getEnv } from "../helpers/getEnv";
 const { VITE_BASE_URL } = getEnv();
 
 export const springBootAxios = axios.create({ baseURL: VITE_BASE_URL });
+const token = localStorage.getItem("token");
 springBootAxios.interceptors.request.use(config => {
     config.headers = {
         ...config.headers,
-        Authorization: `Bearer ${localStorage.getItem("token")}`
+        Authorization: token ? `Bearer ${token}` : null
     };
     return config;
 });
